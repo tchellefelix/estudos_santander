7709c7b4c560faf355b19430354fe6e33cba8711
Marcelle <marcelle.felix@hotmail.com>
2021-07-18 10:52:58 -0300

    Exercício criação de cadastro

--- node.js
+++ node.js
@@ -1,14 +1,19 @@
 class Pessoa{
-  constructor(nome, idade){
-    this.nome = nome;
-    this.idade = idade;
-  }
+    constructor (nome, idade){
+        this.nome = nome;
+        this.idade = idade;
+    }
 }
-const pessoa1 = new Pessoa('Marcelle', 33);
-console.log(pessoa1);
-
-const pessoa2 = new Pessoa('Cássio', 28);
-console.log(pessoa2);
-
-const pessoa3 = new Pessoa('Lucas Henrique', 17);
-console.log(pessoa3);
+class Cidadao extends Pessoa{
+    constructor(nome, idade, cpf, rg){
+        super(nome, idade);
+        this.cpf = cpf;
+        this.rg = rg;
+    }
+}
+const cidadao = new Cidadao('teste', 20, '00099977788', '123456');
+console.log(cidadao);    
+const cidadao2 = new Cidadao('teste2', 23, '00099977788', '123456');
+console.log(cidadao2);
+const cidadao3 = new Cidadao('teste3', 19, '00099977788', '123456');
+console.log(cidadao3);
